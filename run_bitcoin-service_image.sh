ea3e5e2b3063959dddcdf011273d396eb8899fc0
Steve Bradley <steve@debian8svm.debian8svm>
2019-04-13 13:06:26 -0400

    remove run_image script

--- run_bitcoin-service_image.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# run as: ./run_bitcoin-service_image.sh IMAGE_NAME RPC_USER RPC_PASSWORD
-RPCU=RPC_USER=$2
-RPCP=RPC_PASSWORD=$3
-
-docker run --mount source=peanuts,target=/data -e $RPCU -e $RPCP -p 8333:8333 $1
