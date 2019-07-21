1ed4a04851c3d758ad029f20055c1ce0a5954e07
Steve Bradley <steve@debian8svm.debian8svm>
2019-07-21 09:57:34 -0400

    earlier echo

--- run_bitcoin-service.sh
+++ run_bitcoin-service.sh
@@ -6,6 +6,8 @@ do
     ALLOWED_IP=$(dig +short $RPC_CLIENT_HOST)
 done
 
+echo "Allowed IP: $ALLOWED_IP"
+
 sed -i "s/ALLOWED_IP/${ALLOWED_IP}/g" bitcoin.conf
 
 bitcoind -datadir=/data -prune=551 -server -rpcuser=$RPC_USER -rpcpassword=$RPC_PASSWORD -conf=/project/bitcoin.conf
@@ -15,5 +17,3 @@ do
     sleep 1
     SERVER_RESPONSE=$(curl --user $RPC_USER:$RPC_PASSWORD --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "getblockchaininfo", "params": [] }' -H 'content-type: text/plain;' http://127.0.0.1:8332/)
 done
-
-echo "Allowed IP: $ALLOWED_IP"
